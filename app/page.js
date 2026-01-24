6bc74c9067a31ea121cb124c0c616f07708ac6ea
Naila <naipaugue@gmail.com>
2026-01-23 22:58:57 -0700

    Finished styling

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Page(){
       <h1 >
         CPRG 306: Web Development 2 - Assignments
       </h1>
-      <Link href="week-2" className="text-center font-semibold p-3 text-indigo-500 "> Week 2 Assignment </Link>
+      <Link href="week-2" className="text-center"> Week 2 Assignment </Link>
     </main>
   )
 }
--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -5,7 +5,7 @@ export default function StudentInfo() {
         <section>
             <h1>Student Information</h1>
             <h2>Student name: Naila Guerrero </h2>
-            <Link href= "https://github.com/Naila-Guerrero/cprg306-assignments" className="text-center font-semibold"> <strong>Super cool and awesome repo</strong> </Link>
+            <Link href= "https://github.com/Naila-Guerrero/cprg306-assignments" className="text-center"> <strong>Super cool and awesome repo</strong> </Link>
         </section>
     )
 }
